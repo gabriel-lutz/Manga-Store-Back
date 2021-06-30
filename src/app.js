215e93c2f4f68adb94a2a3db9515cdc7742fe778
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-06-30 12:58:40 -0300

    Arrumado conflito de nomes de chaves no método get /allmangas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 
+app.get('/allmangas', async (req,res)=>{
+    try{
+        const query = await connection.query(`
+        SELECT mangas.*, categories.name AS "categoryName" 
+        FROM mangas
+        JOIN categories 
+        ON categories.id = mangas."categoryId"
+        `)
+        res.send(query.rows).status(200)
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
 
 
 export default app
