0d9c208767d2877fb12cae5e9f21d2c907758bd0
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-07-02 16:29:46 -0300

    added env

--- src/database.js
+++ src/database.js
@@ -3,11 +3,11 @@ import pg from "pg"
 const {Pool} = pg
 
 const connection = new Pool({
-    user: 'postgres',
-    password: '123456',
-    host: 'localhost',
-    port: 5432,
-    database: 'mangastore'
+    host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
+	database: process.env.DB_DATABASE
 })
 
 // const connection = new Pool({
